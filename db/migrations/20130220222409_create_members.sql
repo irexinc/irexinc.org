ced1794f334d5ef3f8c8a900ea4d59a266b334ec
Matt Emborsky <memborsky@gmail.com>
2013-02-21 01:06:44 -0500

    Provide the current migrations for the database.

---
+++ db/migrations/20130220222409_create_members.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `members` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `active` binary(1) NOT NULL DEFAULT '0',
+  `board` binary(1) NOT NULL DEFAULT '0',
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `company` text,
+  `city` varchar(50) DEFAULT NULL,
+  `address` text,
+  `state` varchar(2) NOT NULL DEFAULT 'IN',
+  `zip` varchar(10) DEFAULT NULL,
+  `office_phone` varchar(20) DEFAULT NULL,
+  `cell_phone` varchar(20) DEFAULT NULL,
+  `email` text,
+  `officer` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
---
+++ db/migrations/20130220222418_create_events.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `events` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `active` binary(1) NOT NULL DEFAULT '0',
+  `start_date` datetime NOT NULL,
+  `end_date` datetime DEFAULT NULL,
+  `title` text NOT NULL,
+  `location` text NOT NULL,
+  `description` text,
+  `address` text,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
