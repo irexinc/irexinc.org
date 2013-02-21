0d7891543941bf91b9b2de4535203e62e2c8faf7
Matt Emborsky <memborsky@gmail.com>
2013-02-21 15:09:08 -0500

    Provide support for development and production specific config settings. Database and application key specifically.

---
+++ application/config/development/database.php
@@ -0,0 +1,15 @@
+<?php
+
+return array (
+  'connections' => array(
+    'mysql' => array(
+      'driver'   => 'mysql',
+      'host'     => "127.0.0.1",
+      'database' => "irex",
+      'username' => "irex",
+      'password' => "irex",
+      'charset'  => 'utf8',
+      'prefix'   => '',
+    ),
+  ),
+);
