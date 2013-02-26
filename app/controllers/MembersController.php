9d3a40b011aaff2e0acfa2eb3f87b7f4b4e8bf76
Matt Emborsky <memborsky@gmail.com>
2013-02-25 22:28:20 -0500

    Little documentation on mapping Route::resource in MembersController.

--- app/controllers/MembersController.php
+++ app/controllers/MembersController.php
@@ -1,8 +1,22 @@
 <?php
-
+/**
+* Verb      Path                  Action
+* GET       /members              index
+* GET       /members/create       create
+* POST      /members              store
+* GET       /members/{id}         show
+* GET       /members/{id}/edit    edit
+* PUT/PATCH /members/{id}         update
+* DELETE    /members/{id}         destroy
+*/
 class MembersController extends BaseController {
 
-  public function index()
+  /**
+  * GET -> /members
+  *
+  * @return View
+  */
+  public function index ()
   {
     $members = array(
       'board' => Member::where('active', '=', true)->where('board', '=', true)->orderBy('last_name')->get(),
