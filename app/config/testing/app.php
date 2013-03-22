d53c329636bfc64ed565861be1169d7a1fe17581
Matt Emborsky <memborsky@gmail.com>
2013-03-22 11:17:09 -0400

    Support for testing the app.

---
+++ app/config/testing/app.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+  'timezone' => 'America/Indianapolis',
+  'key' => '9fb2840a6f5bdc5c5db987c4b96f6fee'
+);
---
+++ app/config/testing/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+  'connections' => array(
+    'mysql' => array(
+      'driver'    => 'mysql',
+      'host'      => '127.0.0.1',
+      'database'  => 'irex_test',
+      'username'  => 'irex',
+      'password'  => 'irex',
+      'charset'   => 'utf8',
+      'collation' => 'utf8_unicode_ci',
+      'prefix'    => ''
+    ),
+  ),
+
+  'migrations' => 'schema_migrations'
+);
