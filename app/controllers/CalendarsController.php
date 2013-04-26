6da1dd93b6ccbc9a83c633c4b3b16c5924429fa5
Matt Emborsky <memborsky@gmail.com>
2013-04-26 14:58:02 -0400

    Make sure we limit the /calendar events to just those that are marked as active.

--- app/controllers/CalendarsController.php
+++ app/controllers/CalendarsController.php
@@ -4,7 +4,7 @@ class CalendarsController extends BaseController {
 
   public function index()
   {
-    $events = Events::with('calendar')->orderBy('start_date')->orderBy('end_date')->get();
+    $events = Events::with('calendar')->where('active', '=', 1)->orderBy('start_date')->orderBy('end_date')->get();
 
     return View::make('calendar.index')->with('events', $events);
   }
