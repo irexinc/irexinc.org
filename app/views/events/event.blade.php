6c6f716db20f131c34483fe262c3c4d39fa4ba2d
Matt Emborsky <memborsky@gmail.com>
2013-04-25 22:24:34 -0400

    Add support to link non Knights of Columbus locations to their address on google maps.

--- app/views/events/event.blade.php
+++ app/views/events/event.blade.php
@@ -3,6 +3,8 @@
   <td>{{ $event->title }}</td>
   @if (trim($event->location) == "Knights of Columbus")
   <td><a href="#knights-of-columbus">{{ $event->location }}</a></td>
+  @elseif (trim($event->address) != "")
+  <td><a href="https://maps.google.com/maps?q={{{ $event->address }}}&z=12">{{ $event->location }}</a></td>
   @else
   <td>{{ $event->location }}</td>
   @endif
