f3d6c21b1c413bd8f9cec6a913b66a96a3094735
Matt Emborsky <memborsky@gmail.com>
2013-04-25 21:03:24 -0400

    We do development work from a machine named winter as well.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-  'development' => array('black', 'http://localhost*'),
+  'development' => array('black', 'winter', 'http://localhost*'),
   'production' => array('http://irexinc.org/', 'https://irexinc.org/', 'irexinc.org'),
   'test' => array('hooker')
 
