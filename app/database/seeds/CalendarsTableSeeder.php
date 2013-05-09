c930834527c5267f1d29f52d2cf47efba9af8d21
Matt Emborsky <memborsky@gmail.com>
2013-05-09 14:44:26 -0400

    PHP 5.3 does not support short array syntax of [ ]. This reverts it to just array( ).

--- app/database/seeds/CalendarsTableSeeder.php
+++ app/database/seeds/CalendarsTableSeeder.php
@@ -4,10 +4,10 @@ class CalendarsTableSeeder extends Seeder {
 
   public function run()
   {
-    $calendars = [
-      [ 'active' => 1, 'title' => 'IREX Meetings'  ],
-      [ 'active' => 1, 'title' => 'Other Meetings' ],
-    ];
+    $calendars = array(
+      array( 'active' => 1, 'title' => 'IREX Meetings'  ),
+      array( 'active' => 1, 'title' => 'Other Meetings' ),
+    );
 
     DB::table('calendars')->insert($calendars);
   }
