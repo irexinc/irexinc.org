cd8dab179d0577827f93b73d242ccc738fbdc921
Matt Emborsky <memborsky@gmail.com>
2013-05-09 16:09:46 -0400

    Revert event seeding to the previous method so we get created_at and updated_at set correctly without having to create a bunch of new DateTime's ourself.

--- app/database/seeds/EventsTableSeeder.php
+++ app/database/seeds/EventsTableSeeder.php
@@ -18,24 +18,18 @@ class EventsTableSeeder extends Seeder {
       array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-05-23 09:00:00', 'end_date' => '2013-05-23 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Knights of Columbus', 'address' => '1305 North Delaware Street, Indianapolis, IN' ),
       array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-06-13 09:00:00', 'end_date' => '2013-06-13 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Knights of Columbus', 'address' => '1305 North Delaware Street, Indianapolis, IN' ),
       array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-06-27 09:00:00', 'end_date' => '2013-06-27 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Knights of Columbus', 'address' => '1305 North Delaware Street, Indianapolis, IN' ),
-      array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-07-11 09:00:00', 'end_date' => '2013-07-11 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Indianapolis, IN', 'address' => null ),
-      array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-07-25 09:00:00', 'end_date' => '2013-07-25 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Indianapolis, IN', 'address' => null ),
-      array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-08-08 09:00:00', 'end_date' => '2013-08-08 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Indianapolis, IN', 'address' => null ),
-      array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-08-22 09:00:00', 'end_date' => '2013-08-22 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Indianapolis, IN', 'address' => null ),
-      array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-09-12 09:00:00', 'end_date' => '2013-09-12 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Indianapolis, IN', 'address' => null ),
+      array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-07-11 09:00:00', 'end_date' => '2013-07-11 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Indianapolis, IN' ),
+      array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-07-25 09:00:00', 'end_date' => '2013-07-25 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Indianapolis, IN' ),
+      array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-08-08 09:00:00', 'end_date' => '2013-08-08 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Indianapolis, IN' ),
+      array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-08-22 09:00:00', 'end_date' => '2013-08-22 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Indianapolis, IN' ),
+      array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-09-12 09:00:00', 'end_date' => '2013-09-12 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Indianapolis, IN' ),
       array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-09-26 08:00:00', 'end_date' => '2013-09-27 19:00:00', 'title' => 'IREX Fall Marketing Session', 'location' => 'Hilton Garden Inn - Greenwood', 'address' => '5255 Noggle Way, Indianapolis, IN 46237' ),
-      array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-10-10 09:00:00', 'end_date' => '2013-10-10 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Indianapolis, IN', 'address' => null ),
-      array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-10-24 09:00:00', 'end_date' => '2013-10-24 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Indianapolis, IN', 'address' => null ),
-      array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-11-14 09:00:00', 'end_date' => '2013-11-14 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Indianapolis, IN', 'address' => null ),
-      array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-12-12 09:00:00', 'end_date' => '2013-12-12 12:00:00', 'title' => 'IREX Meeting - Holiday Party and Awards Luncheon', 'location' => 'Indianapolis, IN', 'address' => null ),
-    );
+      array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-10-10 09:00:00', 'end_date' => '2013-10-10 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Indianapolis, IN' ),
+      array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-10-24 09:00:00', 'end_date' => '2013-10-24 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Indianapolis, IN' ),
+      array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-11-14 09:00:00', 'end_date' => '2013-11-14 12:00:00', 'title' => 'IREX Meeting', 'location' => 'Indianapolis, IN' ),
+      array( 'calendar_id' => 1, 'active' => 1, 'start_date' => '2013-12-12 09:00:00', 'end_date' => '2013-12-12 12:00:00', 'title' => 'IREX Meeting - Holiday Party and Awards Luncheon', 'location' => 'Indianapolis, IN' ),
 
-    DB::table('events')->insert($events);
-
-
-
-    // Other Meetings/Events
-    $events = array(
+      // Non-IREX Events
       array( 'calendar_id' => 2, 'active' => 1, 'start_date' => '2013-01-20 00:00:00', 'end_date' => '2013-01-23 00:00:00', 'title' => 'Society of Exchange Counselors', 'url' => 'http://www.secounselors.com', 'location' => 'Arizona' ),
       array( 'calendar_id' => 2, 'active' => 1, 'start_date' => '2013-03-17 00:00:00', 'end_date' => '2013-03-20 00:00:00', 'title' => 'Society of Exchange Counselors', 'url' => 'http://www.secounselors.com', 'location' => 'Kansas' ),
       array( 'calendar_id' => 2, 'active' => 1, 'start_date' => '2013-04-28 00:00:00', 'end_date' => '2013-05-01 00:00:00', 'title' => 'KREE Derby City Marketing Session', 'url' => 'http://kree.org', 'location' => 'Louisville, KY' ),
@@ -49,6 +43,9 @@ class EventsTableSeeder extends Seeder {
       array( 'calendar_id' => 2, 'active' => 1, 'start_date' => '2013-11-17 00:00:00', 'end_date' => '2013-11-20 00:00:00', 'title' => 'Society of Exchange Counselors', 'url' => 'http://www.secounselors.com', 'location' => 'California' ),
     );
 
-    DB::table('events')->insert($events);
+    foreach ($events as $event)
+    {
+      Event::create($event);
+    }
   }
 }
