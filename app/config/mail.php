mattboutet/chaliceV3

The default sendmail path must contain '-bs' or '-t' flags.
Signed-off-by: Isern Palaus <ipalaus@ipalaus.es>

app/config/mail.php
@@ -106,6 +106,6 @@ return array(
 	|
 	*/
 
-	'sendmail' => '/usr/sbin/sendmail',
+	'sendmail' => '/usr/sbin/sendmail -bs',
 
 );

irexinc/irexinc.org

Conflicts:
	app/config/mail.php

app/config/mail.php
@@ -106,6 +106,7 @@ return array(
   |
   */
 
-  'sendmail' => '/usr/sbin/sendmail',
+  'sendmail' => '/usr/sbin/sendmail -sb',
+
 
 );