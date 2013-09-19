c6a2151b18f718403de20d99f981e73a29b0aade
Matt Emborsky <memborsky@gmail.com>
2013-09-18 22:24:11 -0400

    Linkify the MIBOR text on the home page.

--- app/views/static/index.blade.php
+++ app/views/static/index.blade.php
@@ -95,7 +95,7 @@ Home &middot; Indiana Real Estate Exchangors, Inc.
     <div class="title">Welcome</div>
 
     <p>We meet the <u>Second (2nd) and Fourth (4th) Thursday</u> of the month. Meetings
-    take place at <strong>MIBOR</strong>.</p>
+    take place at <strong><a href="https://maps.google.com/maps?q=1912%20N.%20Meridian,%20Indianapolis,%20IN&z=12">MIBOR</a></strong>.</p>
 
     <p>Networking starts around 8:45am and the meeting starts promptly at 9:00am.</p>
 
