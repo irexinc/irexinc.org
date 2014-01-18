2461beee0d7ca9d7c9cd25dfeed58afbdd076d17
Matt Emborsky <memborsky@gmail.com>
2014-01-17 19:00:00 -0500

    Karen Lyles speaks January 23rd.

--- app/views/news/index.blade.php
+++ app/views/news/index.blade.php
@@ -25,6 +25,31 @@ Home &middot; Indiana Real Estate Exchangors, Inc.
   @endif
 
   <div class="content-block">
+    <div class="title">Karen Lyles speaks January 23rd</div>
+
+    <p>We're pleased to welcome Karen Lyles from <a href="http://www.vectren.com">Vectren Corporation</a> to our meeting on <u>January 23rd</u>. Karen will be speaking about how to partner with Vectren to use energy wisely. Some of the topics that she will be discussing:</p>
+
+    <ul>
+      <li>How to understand your natural gas and/or electric bill and how costs are determined</li>
+      <li>Appliance rebates on high-efficiency appliances</li>
+      <li>Online tools and resources to analyze your home or businesses' energy use</li>
+      <li>Free and low-cost energy efficiency tips to lower energy usage</li>
+      <li>Billing and payment options to manage costs</li>
+      <li>Information about the natural gas Choice program (Ohio only)</li>
+      <li>Outage response, electric and/or natural gas safety</li>
+    </ul>
+
+    <p>Joining Karen will be one of Vectren's business marketing consultants to talk about <a href="http://www.vectren.com/Save_Energy/Business_Rebates_&amp;_Incentives/Business_Rebates.jsp">opportunities in commercial applications for rebates and reducing utility costs</a>.</p>
+
+    <br>
+
+    <p><strong>A little more information about Karen:</strong></p>
+
+    <p><em>In November 2010, Karen Lyles joined the Sales/Marketing department at Vectren Corporation where she is responsible for new business growth and development of Boone, Carroll, Tippecanoe, Clinton, Montgomery, and White counties.  Prior to working for Vectren, the Gary, Indiana, native served as the Vice President of Programs and Administration for the Center for Leadership Development.</em></p>
+
+  </div>
+
+  <div class="content-block">
     <div class="title">Testimonial</div>
 
     <em>
