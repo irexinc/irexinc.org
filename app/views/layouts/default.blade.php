5b94f9116bd0460975ae88800b302a7fe55e9ae3
Matt Emborsky <memborsky@gmail.com>
2014-03-05 16:46:39 -0500

    Add NCE to the header links for now.

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -65,6 +65,7 @@
               <li><a href="{{ URL::to('calendar') }}">Calendar</a></li>
               <li><a href="{{ URL::to('by-laws') }}">By-Laws</a></li>
               <li><a href="{{ URL::to('documents') }}">Documents</a></li>
+              <li><a href="http://irex.ncexchangors.com/">NCE</a></li>
 
             @show
 
