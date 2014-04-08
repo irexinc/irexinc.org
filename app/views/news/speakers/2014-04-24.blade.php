631b96641ee9595999c963298144b2b987bcafcb
Matt Emborsky <memborsky@gmail.com>
2014-04-07 23:01:21 -0400

    Add some more information about the speakers for the 24th.

--- app/views/news/speakers/2014-04-24.blade.php
+++ app/views/news/speakers/2014-04-24.blade.php
@@ -1,6 +1,6 @@
 <a href="assets/images/new31.jpg" target="_blank"><img src="assets/images/new31.jpg" alt="New US 31 Hamilton County" align="right" /></a>
 
-<p>Coming to us from <a href="http://chacompanies.com" target="_blank">CHA Companies</a> is Jason Rowley, a consultant working for <a href="http://www.in.gov/indot/" target="_blank">INDOT</a>'s <a href="http://us31hamiltoncounty.in.gov" target="_blank">New US 31</a> road project in Hamilton County.</p>
-<p>Joining Jason is Michelle Kearns from <a href="http://www.empowerresults.com">Empower Results</a>.</p>
+<p>Coming to us from <a href="http://chacompanies.com" target="_blank">CHA Companies</a> is Jason Rowley, a consultant working for <a href="http://www.in.gov/indot/" target="_blank">INDOT</a>'s <a href="http://us31hamiltoncounty.in.gov" target="_blank">New US 31</a> road project in Hamilton County. Joining Jason is Michelle Kearns from <a href="http://www.empowerresults.com">Empower Results</a>.</p>
 
 <p>The New US 31 Hamilton County team will provide IREX members with an overview of the project that will upgrade US 31 to freeway standards from I-465 at the Marion-Hamilton county line, through Carmel and Westfield to SR 38. In terms of dollars, this will be the largest and most aggressive road project ever completed in Hamilton County.</p>
+<p>The presentation will include a review of completed and upcoming construction, as well as the opportunity for IREX members to ask specific questions about the project.</p>
