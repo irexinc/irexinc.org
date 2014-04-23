2828f6640ddadbacf9c78bc5824eb72051c7b337
Matt Emborsky <memborsky@gmail.com>
2014-04-23 12:14:38 -0400

    Speakers on May 8th.

---
+++ app/views/speakers/details/2014-05-08_Exchangors-of-the-Years-Panel.blade.php
@@ -0,0 +1,3 @@
+<p>Larry Browning from <a href="http://evergreeninvest.com/">Evergreen Investment Corp</a> was Exchangor of the Year in 2000.</p>
+<p>Clint Fultz from <a href="http://primesitebrokers.com/">Prime Site Brokers</a> has won the Exchangor of the Year in 1993, 1999, 2004, and 2008.</p>
+<p>Ron Tedrow from Tedroco, Inc. won Exchangor of the Year in 2004 and 2013.</p>
