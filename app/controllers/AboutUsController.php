41566271bc9ac53262e3f81b75d2d9d29bd52fa4
Matt Emborsky <memborsky@gmail.com>
2014-05-02 10:46:34 -0400

    We don't need the events table in the About Us stuff.

--- app/controllers/AboutUsController.php
+++ app/controllers/AboutUsController.php
@@ -3,24 +3,6 @@
 class AboutUsController extends BaseController {
 
   /**
-  * The events implementation.
-  *
-  * @var Static
-  */
-  protected $events;
-
-  /**
-  * Set our Events instance.
-  *
-  * @param Events $events
-  * @return void
-  */
-  public function __construct(Events $events)
-  {
-    $this->events = $events;
-  }
-
-  /**
   * GET -> http://irexinc.org/by-laws
   *
   * @return view
