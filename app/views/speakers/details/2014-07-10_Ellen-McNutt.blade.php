53dd08088ffca000054ff83d743618c86cab127b
Matt Emborsky <memborsky@gmail.com>
2014-05-21 16:48:05 -0500

    Speaker: Ellen McNutt

---
+++ app/views/speakers/details/2014-07-10_Ellen-McNutt.blade.php
@@ -0,0 +1,1 @@
+<p>Ellen McNutt is with <a href="http://www.uzelactax.com">Uzelac &amp; Associates, Inc.</a> She will be speaking about how to ensure your Property taxes are correct and fairly assessed. Learn more about what your rights are on appealing your Property Taxes.</p>
