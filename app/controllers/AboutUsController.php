ec5b1777de357a341d0f1d0c2a586cbc2f743bfb
Matt Emborsky <memborsky@gmail.com>
2014-05-24 17:48:31 -0500

    Make the contact page actually do something when you click submit.

--- app/controllers/AboutUsController.php
+++ app/controllers/AboutUsController.php
@@ -21,4 +21,27 @@ class AboutUsController extends BaseController {
   {
     return View::make('about.contact');
   }
+
+  /**
+  * POST -> http://irexinc.org/contact
+  *
+  * @return reroute to /
+  */
+  public function postContact ()
+  {
+    $data = Input::all();
+
+    // Add the current time for displaying in the email.
+    $data['time'] = date('F, l j, Y') . " at " . date('h:i:s A');
+
+    Mail::queue(array('text' => 'emails.contact'), array('request' => $data), function($message) use ($data)
+    {
+      $message->to($_ENV['CONTACT_REQUEST_EMAIL'])
+        ->from($data['email'], $data['name'])
+        ->subject('[irexinc.org] Contact Request from ' . $data['name'] . ' :: ' . $data['subject']);
+    });
+
+    return "true";
+
+  }
 }
--- app/views/about/contact.blade.php
+++ app/views/about/contact.blade.php
@@ -6,42 +6,41 @@ Contact &middot; Indiana Real Estate Exchangors, Inc.
 
 @section('content')
 
-  @if (!empty($success))
-    <div class="content-block green">
-      <p>Contact successfully submitted.</p>
-    </div>
-  @endif
+  <div class="content-block green success" id="success-message">
+    <p>Contact successfully submitted.</p>
+  </div>
 
   <div class="content-block">
-    {{ Form::open(array('url' => 'contact', 'novalidate')) }}
+
+    {{ Form::open(array('id' => 'contact-form', 'url' => 'contact', 'novalidate')) }}
       <dl class="form">
 
         <dt class="input-label">
           <label for="name">Name:</label>
         </dt>
         <dd >
-          <input type="text" name="name" id="name" autofocus placeholder="Your Name"/>
+          <input type="text" name="name" id="name" data-validation="required" />
         </dd>
 
         <dt class="input-label">
           <label for="email">E-Mail:</label>
         </dt>
         <dd>
-          <input type="email" name="email" id="email" required placeholder="example@example.com" />
+          <input type="email" name="email" id="email" placeholder="example@example.com" data-validation="email" />
         </dd>
 
         <dt class="input-label">
           <label for="subject">Subject:</label>
         </dt>
         <dd>
-          <input type="text" name="subject" id="subject" placeholder="Subject of Contact Request (Can be left blank)." />
+          <input type="text" name="subject" id="subject" />
         </dd>
 
         <dt class="input-label">
           <label for="comments">Body:</label>
         </dt>
         <dd>
-          <textarea name="comments" id="comments" required placeholder="Details about your contact request."></textarea>
+          <textarea name="comments" id="comments" placeholder="Details about your contact request." data-validation="required"></textarea>
         </dd>
 
       </dl>
@@ -51,4 +50,30 @@ Contact &middot; Indiana Real Estate Exchangors, Inc.
       </div>
     {{ Form::close() }}
   </div>
+@stop
+
+@section('javascript')
+<script>
+  $.validate({
+    form : '#contact-form',
+    validateOnBlur : true,
+    errorMessagePosition : 'top',
+    scrollToTopOnError : false,
+    onSuccess : function() {
+      $.ajax({
+        url : '/contact',
+        type : 'post',
+        data : $('#contact-form').serialize(),
+        success : function() {
+          // Reset the contact form so it looks like we 'reloaded' the page.
+          $('#contact-form').get(0).reset();
+
+          // Display the success message.
+          $('#success-message').slideDown("slow").delay(3000).slideUp("slow");
+        }
+      });
+      return false;
+    }
+  });
+</script>
 @stop
