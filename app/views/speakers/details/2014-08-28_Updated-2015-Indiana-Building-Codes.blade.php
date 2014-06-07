d0b6bc6b9eb4cc4e4bd07025ffe8f126461b25e2
Matt Emborsky <memborsky@gmail.com>
2014-06-06 21:36:07 -0500

    Speaker 2014-08-28.

---
+++ app/views/speakers/details/2014-08-28_Updated-2015-Indiana-Building-Codes.blade.php
@@ -0,0 +1,1 @@
+<p>Coming to us from <a href="http://fpc-consultants.com/" target="_blank">FP&amp;C Consultants, Inc.</a> is Dennis Bradshaw. He will be speaking about major updates in the 2015 Indiana Building Code.</p>
