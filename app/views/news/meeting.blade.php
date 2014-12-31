39ce802c3e5740a3f51ee190f0aaa884eaa2794b
Matt Emborsky <memborsky@gmail.com>
2014-12-31 11:26:04 -0500

    Do not attempt to send users to google maps for a meeting that has no set address yet.

--- app/views/news/meeting.blade.php
+++ app/views/news/meeting.blade.php
@@ -1,7 +1,11 @@
 <div class="next-meeting-container">
+  @if (!empty($meeting['address']))
     <h4>{{ $meeting['date'] }} at <a href="https://www.google.com/maps/preview/place/{{ $meeting['address'] }}" target="_blank">{{ $meeting['location'] }}</a>.</h4>
+  @else
+    <h4>{{ $meeting['date'] }} at {{ $meeting['location'] }}.</h4>
+  @endif
 
-    @if (!empty($meeting['speaker']))
-    @include($meeting['speaker'])
-    @endif
+  @if (!empty($meeting['speaker']))
+  @include($meeting['speaker'])
+  @endif
 </div>
