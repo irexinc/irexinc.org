3df3ed7edeacd104a799256cd336f2430633b123
Matt Emborsky <memborsky@gmail.com>
2015-01-08 00:09:29 -0500

    Lets see if this doesn't break travis builds more.

--- app/config/testing/app.php
+++ app/config/testing/app.php
@@ -1,6 +1,3 @@
 <?php
 
-return array(
-  'timezone' => 'America/Indianapolis',
-  'key' => '9fb2840a6f5bdc5c5db987c4b96f6fee'
-);
+return array( );
--- app/config/testing/database.php
+++ app/config/testing/database.php
@@ -1,18 +1,3 @@
 <?php
 
-return array(
-  'connections' => array(
-    'mysql' => array(
-      'driver'    => 'mysql',
-      'host'      => '127.0.0.1',
-      'database'  => 'irex_test',
-      'username'  => 'root',
-      'password'  => '',
-      'charset'   => 'utf8',
-      'collation' => 'utf8_unicode_ci',
-      'prefix'    => ''
-    ),
-  ),
-
-  'migrations' => 'schema_migrations'
-);
+return array( );
