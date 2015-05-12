c7f8afcfc34f86b34dc0a7c692684f1b677b53b5
Matt Emborsky <memborsky@gmail.com>
2015-05-12 14:43:05 -0400

    Update speaker details

--- app/views/speakers/details/2015-05-14_Sperry-Van-Ness.blade.php
+++ app/views/speakers/details/2015-05-14_Sperry-Van-Ness.blade.php
@@ -1,1 +1,1 @@
-<p>Harry Kennerk from <a href="http://svn.com/">Sperry Van Ness</a> will be speaking.</p>
+<p>Harry Kennerk from <a href="http://svn.com/">Sperry Van Ness</a> will be speaking about When the Solution to the Problem is a System.</p>
